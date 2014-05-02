2b0a91550b4089ec645b007db1cd3e234e9f86f0
Maxime Burriez <maxime@tymate.com>
2014-05-02 16:56:37 +0200

    first commit

--- lib/pmv_manager.rb
+++ lib/pmv_manager.rb
@@ -15,16 +15,15 @@ module PmvManager
       @pmv_address = pmv_address # integer
       @pmv_ip = pmv_ip           # string
       @pmv_port = pmv_port       # integer
-      # @socket = UDPSocket.new    # UDPSocket
+      @socket = UDPSocket.new    # UDPSocket
     end
-    # def socket
-    #   @socket
-    # end
+    def socket
+      @socket
+    end
     def send(command)
       packaged_command = package(command)
-      socket = UDPSocket.new
-      socket.send packaged_command, 0, @ip, @pmv_port
-      socket.recv PmvManager::MAX_PACKET_SIZE
+      @socket.send packaged_command, 0, @ip, @pmv_port
+      @socket.recvfrom_nonblock PmvManager::MAX_PACKET_SIZE
     end
     def package(command)
       p = ("\x02#{[@pmv_address.to_s(16)].pack("H*")}#{command.definition}\x03").unpack("C*")
